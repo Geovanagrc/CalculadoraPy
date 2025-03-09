25e6db50d1b27405b2e36f74127bde581a75cad1
Geovana Garcia dos Santos <geovanagrc2005@gmail.com>
2025-03-08 22:16:16 -0300

    Configuracoes iniciais

---
+++ Calculadorasimpls.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+
+janela = tk.Tk()
+janela.geometry("600x800")
+janela.title("Calculadora simples")
+janela.resizable(0,0)
+janela.iconbitmap("Calculadora.ico")
+
+
+
+
+
+
+janela.mainloop()
