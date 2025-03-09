18da2892256f9c7b13b55652fba842ded0433cdd
Geovana Garcia dos Santos <geovanagrc2005@gmail.com>
2025-03-08 23:07:09 -0300

    Logica do botao

--- Calculadorasimpls.py
+++ Calculadorasimpls.py
@@ -1,15 +1,26 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import *
+
+numeros = ["0","1","2","3","4","5","6","7","8","9"]
+ops = ["+","-","*","/"]
+
+def clicou (*args):
+    entrada.config(text = "1")
 
 janela = tk.Tk()
-janela.geometry("600x800")
+janela.geometry("300x400")
 janela.title("Calculadora simples")
 janela.resizable(0,0)
 janela.iconbitmap("Calculadora.ico")
 
+barra = LabelFrame(janela, text = "Calculadora") 
+barra.place(x=0, y =0 )
+barra.config (height=60,width=300)
 
+entrada = Label(janela,text="Insira um número")
+entrada.place(x=0, y = 25)
 
-
-
+botao1 = Button(janela,text="1",command = clicou)
+botao1.place(x=0, y = 60)
 
 janela.mainloop()
